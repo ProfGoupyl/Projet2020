459340f0e2185cce9032d5e0e7ac9a3b53f0de15
Waryen <jonathangomand@gmail.com>
2020-11-25 17:59:50 +0100

    test vue.js

---
+++ front-end/js/app-jo.js
@@ -0,0 +1,11 @@
+const jo = new Vue({
+    el: "#app",
+    data: {
+        message: "Hello World !",
+        students: [
+            {name: 'John'},
+            {name: 'Nico'},
+            {name: 'Antoine'}
+        ]
+    }
+})
