da933d583bc6fe34150e3d79dc092571df4cc583
xspray <propagepress@hotmail.com>
2020-11-28 19:40:51 +0100

    Création des migrations et de la db

---
+++ projet_2020/database/migrations/2020_11_28_180351_create_users_cours_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersCoursTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users_cours', function (Blueprint $table) {
+            $table->id();
+            $table->date('start_at');
+            $table->date('end_at');
+            $table->integer('users_id');
+            $table->integer('cours_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users_cours');
+    }
+}
