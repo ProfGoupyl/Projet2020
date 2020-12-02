0b1ba9d807b77d79b59b63c33eab7a86d16ed6f2
Waryen <jonathangomand@gmail.com>
2020-12-02 14:21:59 +0100

    ajout composant home - ajout layout default

---
+++ projet_2020/resources/js/components/home.component.vue
@@ -0,0 +1,17 @@
+<template>
+    <h1>Accueil</h1>
+</template>
+
+<script>
+export default {
+    mounted() {
+        console.log('Hello')
+    }
+}
+</script>
+
+<style>
+    h1 {
+        font-size: 3em;
+    }
+</style>
