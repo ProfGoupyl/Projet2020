1515ba6e78036d55d9fe04471b0639f6ea8b93cd
Morphee-creator <milican.2405.student@ifosupwavre.be>
2020-12-07 16:07:57 +0100

    Modif header pas encore dans la bonne page (a venir)

--- projet_2020/resources/views/includes/header.blade.php
+++ projet_2020/resources/views/includes/header.blade.php
@@ -3,9 +3,9 @@
         <a id="logo" href="/">Front test</a>
         <ul class="nav">
             <li><a href="/">Home</a></li>
-            <li><a href="/about">About</a></li>
-            <li><a href="/projects">Projects</a></li>
-            <li><a href="/contact">Contact</a></li>
+            <li><a href="/faq">Faq</a></li>
+            <li><a href="/profile">Profile</a></li>
+            <li><a href="/session">Cours</a></li>
         </ul>
     </div>
 </div>
