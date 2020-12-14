518d66a18075bff996f0d87395766387b1befe1a
Waryen <jonathangomand@gmail.com>
2020-12-14 17:04:42 +0100

    indentation composant user

--- projet_2020/resources/js/components/user.component.vue
+++ projet_2020/resources/js/components/user.component.vue
@@ -21,15 +21,15 @@
             }
         },
         mounted() {
-                axios
-                    .get('http://localhost:8000/api/cours?api_token=sxSVzOnXPDZRk0UFuDMKhaMV2TC5accFVar9epV5nkxiIigOJ08AkFFs5HmkwxIYZ10e1cj1dZGDZIxFg6p4s9a0B8oS2c0bU3o9')
-                    .then(response => (this.coursList = response.data))
-                    .catch(error => console.log(error))
-                
-                axios
-                    .get('http://localhost:8000/api/users_cours?api_token=sxSVzOnXPDZRk0UFuDMKhaMV2TC5accFVar9epV5nkxiIigOJ08AkFFs5HmkwxIYZ10e1cj1dZGDZIxFg6p4s9a0B8oS2c0bU3o9')
-                    .then(response => (this.coursUsers = response.data))
-                    .catch(error => console.log(error))
+            axios
+                .get('http://localhost:8000/api/cours?api_token=sxSVzOnXPDZRk0UFuDMKhaMV2TC5accFVar9epV5nkxiIigOJ08AkFFs5HmkwxIYZ10e1cj1dZGDZIxFg6p4s9a0B8oS2c0bU3o9')
+                .then(response => (this.coursList = response.data))
+                .catch(error => console.log(error))
+            
+            axios
+                .get('http://localhost:8000/api/users_cours?api_token=sxSVzOnXPDZRk0UFuDMKhaMV2TC5accFVar9epV5nkxiIigOJ08AkFFs5HmkwxIYZ10e1cj1dZGDZIxFg6p4s9a0B8oS2c0bU3o9')
+                .then(response => (this.coursUsers = response.data))
+                .catch(error => console.log(error))
         }
     }
 </script>
