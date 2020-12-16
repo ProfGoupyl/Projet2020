7d8887a464a4c76a4a51d212d31f7ecd1af2f698
xspray <propagepress@hotmail.com>
2020-12-16 21:21:48 +0100

    Modification csv

--- projet_2020/resources/views/administration/Csv.blade.php
+++ projet_2020/resources/views/administration/Csv.blade.php
@@ -1,11 +1,13 @@
 @extends('layouts.default')
 @section('content')
+
 <p>Test ajout fichier csv</p>
 <p>Le formulaire d'ajout d'un fichier .csv devrait se faire dans la vue d'inscription des administrateurs.</p>
 
-<form action="" method="get">
-  
+<form action="csv" method="post">
+  @csrf
+  <input type="file" name="myFile" value="ouvrir">
   <input type="submit" name="submit" value="Test lecture csv" title="Submit">
-</form>
+</form> 
 
 @stop
