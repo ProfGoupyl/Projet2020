128a408db5108eb18caf85aa6d5e69578cec3141
RomainCE <romain.scorneaux@hotmail.com>
2020-12-18 16:14:16 +0100

    Formulaire csv terminé (il reste à injecter les données dans la db).

--- projet_2020/app/Http/Controllers/CsvController.php
+++ projet_2020/app/Http/Controllers/CsvController.php
@@ -1,21 +1,20 @@
 <?php
-
 namespace App\Http\Controllers;
-
 use Illuminate\Http\Request;
-
+use resources\csv;
 class CsvController extends Controller{
-
   public function index(Request $request){
-
     return view('administration.csv');
-
   }
-
   public function get_csv(Request $request){
-
-    return view('administration.csv');
-
+    $res=fopen(request('file'), "r");
+    while(!feof($res)){
+      $tabLigne=explode(';', fgets($res));
+      foreach($tabLigne as &$v){
+        echo $v.'<br>';
+      }
+      echo '<br>';
+    }
+    fclose($res);
   }
-
 }
--- projet_2020/resources/views/administration/Csv.blade.php
+++ projet_2020/resources/views/administration/Csv.blade.php
@@ -4,10 +4,10 @@
 <p>Test ajout fichier csv</p>
 <p>Le formulaire d'ajout d'un fichier .csv devrait se faire dans la vue d'inscription des administrateurs.</p>
 
-<form action="csv" method="post">
+<form enctype="multipart/form-data" action="csv" method="post">
   @csrf
-  <input type="file" name="myFile" value="ouvrir">
-  <input type="submit" name="submit" value="Test lecture csv" title="Submit">
+  <input required type="file" name="file" value="Choisir un fichier .csv" accept=".csv">
+  <input type="submit" name="submit" value="Transférer" title="Submit">
 </form>
 
 @stop
