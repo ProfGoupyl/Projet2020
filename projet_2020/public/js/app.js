00be7b7cc8c681959eec5991d7ddf6c96216d356
Morphee-creator <milican.2405.student@ifosupwavre.be>
2021-01-11 15:49:48 +0100

    clear id module

--- projet_2020/public/js/app.js
+++ projet_2020/public/js/app.js
@@ -3887,7 +3887,7 @@ __webpack_require__.r(__webpack_exports__);
   props: ['userid'],
   data: function data() {
     return {
-      userId: 37,
+      userId: 99,
       coursId: JSON.parse(sessionStorage.getItem('coursid')),
       moduleList: [],
       coursList: [],
@@ -4130,9 +4130,8 @@ __webpack_require__.r(__webpack_exports__);
       return console.log(error);
     });
   },
-  // beforeDestroy ne fonctionne pas
-  beforeDestroy: function beforeDestroy() {
-    this.moduleId = null;
+  // permet de clear l'id du module
+  updated: function updated() {
     sessionStorage.removeItem('moduleid');
   }
 });
@@ -52840,6 +52839,17 @@ __webpack_require__.r(__webpack_exports__);
 
 /***/ }),
 
+/***/ "./resources/sass/app.scss":
+/*!*********************************!*\
+  !*** ./resources/sass/app.scss ***!
+  \*********************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+// removed by extract-text-webpack-plugin
+
+/***/ }),
+
 /***/ "./resources/sass/default.scss":
 /*!*************************************!*\
   !*** ./resources/sass/default.scss ***!
@@ -52852,15 +52862,16 @@ __webpack_require__.r(__webpack_exports__);
 /***/ }),
 
 /***/ 0:
-/*!*****************************************************************************************!*\
-  !*** multi ./resources/js/app.js ./resources/sass/default.scss ./resources/css/app.css ***!
-  \*****************************************************************************************/
+/*!*******************************************************************************************************************!*\
+  !*** multi ./resources/js/app.js ./resources/sass/app.scss ./resources/sass/default.scss ./resources/css/app.css ***!
+  \*******************************************************************************************************************/
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /Applications/MAMP/htdocs/ifosup/projet-web-dynamique/Projet2020/projet_2020/resources/js/app.js */"./resources/js/app.js");
-__webpack_require__(/*! /Applications/MAMP/htdocs/ifosup/projet-web-dynamique/Projet2020/projet_2020/resources/sass/default.scss */"./resources/sass/default.scss");
-module.exports = __webpack_require__(/*! /Applications/MAMP/htdocs/ifosup/projet-web-dynamique/Projet2020/projet_2020/resources/css/app.css */"./resources/css/app.css");
+__webpack_require__(/*! C:\wamp64\www\NicolasM\Projet2020\projet_2020\resources\js\app.js */"./resources/js/app.js");
+__webpack_require__(/*! C:\wamp64\www\NicolasM\Projet2020\projet_2020\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\wamp64\www\NicolasM\Projet2020\projet_2020\resources\sass\default.scss */"./resources/sass/default.scss");
+module.exports = __webpack_require__(/*! C:\wamp64\www\NicolasM\Projet2020\projet_2020\resources\css\app.css */"./resources/css/app.css");
 
 
 /***/ })
