4c845fadfdfa2f32271598efbc3c61b03558fe02
Waryen <jonathangomand@gmail.com>
2021-01-11 14:11:04 +0100

    retrait des header dans les composants

--- projet_2020/public/js/app.js
+++ projet_2020/public/js/app.js
@@ -3879,26 +3879,6 @@ __webpack_require__.r(__webpack_exports__);
 //
 //
 //
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
 
 /* harmony default export */ __webpack_exports__["default"] = ({
   components: {
@@ -3976,26 +3956,6 @@ __webpack_require__.r(__webpack_exports__);
 //
 //
 //
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
 /* harmony default export */ __webpack_exports__["default"] = ({
   data: function data() {
     return {
@@ -4024,26 +3984,6 @@ __webpack_require__.r(__webpack_exports__);
 
 "use strict";
 __webpack_require__.r(__webpack_exports__);
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
 //
 //
 //
@@ -4208,26 +4148,6 @@ __webpack_require__.r(__webpack_exports__);
 
 "use strict";
 __webpack_require__.r(__webpack_exports__);
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
 //
 //
 //
@@ -39887,8 +39807,6 @@ var render = function() {
   var _h = _vm.$createElement
   var _c = _vm._self._c || _h
   return _c("div", [
-    _vm._m(0),
-    _vm._v(" "),
     _c("section", [
       _c("aside", [
         _c("nav", { staticClass: "navSecondaire" }, [
@@ -39966,35 +39884,7 @@ var render = function() {
     ])
   ])
 }
-var staticRenderFns = [
-  function() {
-    var _vm = this
-    var _h = _vm.$createElement
-    var _c = _vm._self._c || _h
-    return _c("header", [
-      _c("figure", [
-        _c("a", { staticClass: "UserName", attrs: { href: "/profile" } }, [
-          _c("img", {
-            staticClass: "UserImage",
-            attrs: { src: "", alt: "", height: "80px", width: "80px" }
-          })
-        ])
-      ]),
-      _vm._v(" "),
-      _c("nav", { staticClass: "navPrincipale" }, [
-        _c("ul", { staticClass: "navigation" }, [
-          _c("li", [_c("a", { attrs: { href: "/user" } }, [_vm._v("Cours")])]),
-          _vm._v(" "),
-          _c("li", [_c("a", { attrs: { href: "/faq" } }, [_vm._v("FAQ")])]),
-          _vm._v(" "),
-          _c("li", [
-            _c("a", { attrs: { href: "/logout" } }, [_vm._v("Logout")])
-          ])
-        ])
-      ])
-    ])
-  }
-]
+var staticRenderFns = []
 render._withStripped = true
 
 
@@ -40017,8 +39907,6 @@ var render = function() {
   var _h = _vm.$createElement
   var _c = _vm._self._c || _h
   return _c("div", [
-    _vm._m(0),
-    _vm._v(" "),
     _c("section", [
       _c("h1", [_vm._v("FAQ")]),
       _vm._v(" "),
@@ -40041,35 +39929,7 @@ var render = function() {
     ])
   ])
 }
-var staticRenderFns = [
-  function() {
-    var _vm = this
-    var _h = _vm.$createElement
-    var _c = _vm._self._c || _h
-    return _c("header", [
-      _c("figure", [
-        _c("a", { staticClass: "UserName", attrs: { href: "/profile" } }, [
-          _c("img", {
-            staticClass: "UserImage",
-            attrs: { src: "", alt: "", height: "80px", width: "80px" }
-          })
-        ])
-      ]),
-      _vm._v(" "),
-      _c("nav", { staticClass: "navPrincipale" }, [
-        _c("ul", { staticClass: "navigation" }, [
-          _c("li", [_c("a", { attrs: { href: "/user" } }, [_vm._v("Cours")])]),
-          _vm._v(" "),
-          _c("li", [_c("a", { attrs: { href: "/faq" } }, [_vm._v("FAQ")])]),
-          _vm._v(" "),
-          _c("li", [
-            _c("a", { attrs: { href: "/logout" } }, [_vm._v("Logout")])
-          ])
-        ])
-      ])
-    ])
-  }
-]
+var staticRenderFns = []
 render._withStripped = true
 
 
@@ -40092,8 +39952,6 @@ var render = function() {
   var _h = _vm.$createElement
   var _c = _vm._self._c || _h
   return _c("div", [
-    _vm._m(0),
-    _vm._v(" "),
     _c("section", [
       _c("h1", [_vm._v("Votre profil:")]),
       _vm._v(" "),
@@ -40194,9 +40052,9 @@ var render = function() {
             })
           ]),
           _vm._v(" "),
-          _vm._m(1),
+          _vm._m(0),
           _vm._v(" "),
-          _vm._m(2),
+          _vm._m(1),
           _vm._v(" "),
           _c(
             "button",
@@ -40224,33 +40082,6 @@ var render = function() {
   ])
 }
 var staticRenderFns = [
-  function() {
-    var _vm = this
-    var _h = _vm.$createElement
-    var _c = _vm._self._c || _h
-    return _c("header", [
-      _c("figure", [
-        _c("a", { staticClass: "UserName", attrs: { href: "" } }, [
-          _c("img", {
-            staticClass: "UserImage",
-            attrs: { src: "", alt: "", height: "80px", width: "80px" }
-          })
-        ])
-      ]),
-      _vm._v(" "),
-      _c("nav", { staticClass: "navPrincipale" }, [
-        _c("ul", { staticClass: "navigation" }, [
-          _c("li", [_c("a", { attrs: { href: "/user" } }, [_vm._v("Cours")])]),
-          _vm._v(" "),
-          _c("li", [_c("a", { attrs: { href: "/faq" } }, [_vm._v("FAQ")])]),
-          _vm._v(" "),
-          _c("li", [
-            _c("a", { attrs: { href: "/logout" } }, [_vm._v("Logout")])
-          ])
-        ])
-      ])
-    ])
-  },
   function() {
     var _vm = this
     var _h = _vm.$createElement
@@ -40337,13 +40168,11 @@ var render = function() {
   var _h = _vm.$createElement
   var _c = _vm._self._c || _h
   return _c("div", [
-    _vm._m(0),
-    _vm._v(" "),
     _c("section", [
       _c("h1", [_vm._v("Liste des cours")]),
       _vm._v(" "),
       _c("table", { staticClass: "listecours" }, [
-        _vm._m(1),
+        _vm._m(0),
         _vm._v(" "),
         _c(
           "tbody",
@@ -40385,33 +40214,6 @@ var render = function() {
   ])
 }
 var staticRenderFns = [
-  function() {
-    var _vm = this
-    var _h = _vm.$createElement
-    var _c = _vm._self._c || _h
-    return _c("header", [
-      _c("figure", [
-        _c("a", { staticClass: "UserName", attrs: { href: "/profile" } }, [
-          _c("img", {
-            staticClass: "UserImage",
-            attrs: { src: "", alt: "", height: "80px", width: "80px" }
-          })
-        ])
-      ]),
-      _vm._v(" "),
-      _c("nav", { staticClass: "navPrincipale" }, [
-        _c("ul", { staticClass: "navigation" }, [
-          _c("li", [_c("a", { attrs: { href: "/cours" } }, [_vm._v("Cours")])]),
-          _vm._v(" "),
-          _c("li", [_c("a", { attrs: { href: "/faq" } }, [_vm._v("FAQ")])]),
-          _vm._v(" "),
-          _c("li", [
-            _c("a", { attrs: { href: "/logout" } }, [_vm._v("Logout")])
-          ])
-        ])
-      ])
-    ])
-  },
   function() {
     var _vm = this
     var _h = _vm.$createElement
@@ -53038,17 +52840,6 @@ __webpack_require__.r(__webpack_exports__);
 
 /***/ }),
 
-/***/ "./resources/sass/app.scss":
-/*!*********************************!*\
-  !*** ./resources/sass/app.scss ***!
-  \*********************************/
-/*! no static exports found */
-/***/ (function(module, exports) {
-
-// removed by extract-text-webpack-plugin
-
-/***/ }),
-
 /***/ "./resources/sass/default.scss":
 /*!*************************************!*\
   !*** ./resources/sass/default.scss ***!
@@ -53061,16 +52852,15 @@ __webpack_require__.r(__webpack_exports__);
 /***/ }),
 
 /***/ 0:
-/*!*******************************************************************************************************************!*\
-  !*** multi ./resources/js/app.js ./resources/sass/app.scss ./resources/sass/default.scss ./resources/css/app.css ***!
-  \*******************************************************************************************************************/
+/*!*****************************************************************************************!*\
+  !*** multi ./resources/js/app.js ./resources/sass/default.scss ./resources/css/app.css ***!
+  \*****************************************************************************************/
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\wamp64\www\NicolasM\Projet2020\projet_2020\resources\js\app.js */"./resources/js/app.js");
-__webpack_require__(/*! C:\wamp64\www\NicolasM\Projet2020\projet_2020\resources\sass\app.scss */"./resources/sass/app.scss");
-__webpack_require__(/*! C:\wamp64\www\NicolasM\Projet2020\projet_2020\resources\sass\default.scss */"./resources/sass/default.scss");
-module.exports = __webpack_require__(/*! C:\wamp64\www\NicolasM\Projet2020\projet_2020\resources\css\app.css */"./resources/css/app.css");
+__webpack_require__(/*! /Applications/MAMP/htdocs/ifosup/projet-web-dynamique/Projet2020/projet_2020/resources/js/app.js */"./resources/js/app.js");
+__webpack_require__(/*! /Applications/MAMP/htdocs/ifosup/projet-web-dynamique/Projet2020/projet_2020/resources/sass/default.scss */"./resources/sass/default.scss");
+module.exports = __webpack_require__(/*! /Applications/MAMP/htdocs/ifosup/projet-web-dynamique/Projet2020/projet_2020/resources/css/app.css */"./resources/css/app.css");
 
 
 /***/ })
--- projet_2020/resources/js/components/user.component.vue
+++ projet_2020/resources/js/components/user.component.vue
@@ -1,25 +1,5 @@
 <template>
     <div>
-       <header>
-           <figure>
-               <a href="/profile" class="UserName">
-                    <img src="" alt="" class="UserImage" height="80px" width="80px">
-               </a>
-           </figure>
-           <nav class="navPrincipale">
-               <ul class="navigation">
-                   <li>
-                       <a href="/cours">Cours</a>
-                   </li>
-                   <li>
-                       <a href="/faq">FAQ</a>
-                   </li>
-                   <li>
-                       <a href="/logout">Logout</a>
-                   </li>
-               </ul>
-           </nav>
-       </header>
        <section>
            <h1>Liste des cours</h1>
            <table class="listecours">
