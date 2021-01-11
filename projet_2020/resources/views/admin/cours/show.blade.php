639bd1053fc9ab4ec655e7c1c70eb4999675da52
HugoGuerrera <guerrera.1704.student@ifosupwavre.be>
2021-01-11 13:20:17 +0100

    pull

--- projet_2020/resources/views/admin/cours/show.blade.php
+++ projet_2020/resources/views/admin/cours/show.blade.php
@@ -1,13 +1,47 @@
+<style>
+section.aside{
+    color:red;
+    width: 25%;
+}
+
+section.main{
+    
+    color:violet;
+    width:70%;
+    
+}
+
+div{
+    display:flex;
+    justify-content:space-between;
+}
+
+
+</style>
+
+
+
 <h1>salut {{$cours->titre}}</h1>
-<section>
+<div>
+<section class='aside'>
     @foreach($modules as $m)
     <p>{{$m->titre}}</p>
     @endforeach
+</section>
 
-
-
+<section class='main'>
+    @foreach($modules as $m)
+    <p>{{$m->titre}}</p>
+    <p>{{$m->description}}</p>
+    <p>{{$m->url_video}}</p>
+    
+    @endforeach
 
 </section>
+
+
+</div>
+
 
 
 
