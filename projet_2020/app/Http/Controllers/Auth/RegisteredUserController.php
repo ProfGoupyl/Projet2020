1215b2fdbefdbdc0261544105306108f212e3cc2
xspray <propagepress@hotmail.com>
2021-01-16 16:43:30 +0100

    Remise en route de register

--- projet_2020/app/Http/Controllers/Auth/RegisteredUserController.php
+++ projet_2020/app/Http/Controllers/Auth/RegisteredUserController.php
@@ -2,16 +2,13 @@
 
 namespace App\Http\Controllers\Auth;
 
-use DateTime;
-use App\Models\User;
-use Illuminate\Support\Str;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
 use App\Http\Controllers\Controller;
+use App\Models\User;
+use App\Providers\RouteServiceProvider;
+use Illuminate\Auth\Events\Registered;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
-use Illuminate\Auth\Events\Registered;
-use App\Providers\RouteServiceProvider;
 
 class RegisteredUserController extends Controller
 {
@@ -28,7 +25,7 @@ class RegisteredUserController extends Controller
     /**
      * Handle an incoming registration request.
      *
-     * @param  \App\Http\Requests\LoginRequest  $request
+     * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\RedirectResponse
      *
      * @throws \Illuminate\Validation\ValidationException
@@ -36,37 +33,21 @@ class RegisteredUserController extends Controller
     public function store(Request $request)
     {
         $request->validate([
-            // 'name' => 'required|string|max:255',
-            // 'prenom' => 'required|string|max:255',
-            // 'email' => 'required|string|email|max:255|unique:users',
+            'name' => 'required|string|max:255',
+            'prenom' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
             'password' => 'required|string|confirmed|min:8',
         ]);
 
-        $user = User::where('register_token', '=', $request->token)
-            ->first();
+        Auth::login($user = User::create([
+            'name' => $request->name,
+            'prenom' => $request->prenom,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]));
 
-            //  dump($request->token);
-            //  dd($user->cours[1]->titre);
+        event(new Registered($user));
 
-        $now = new DateTime();
-
-        if ($request->has('pseudo') && $request->filled('pseudo')) {
-            $user->pseudo = $request->pseudo;
-        } else {
-            $user->pseudo = $user->prenom;
-        }
-
-        $user->name = $user->name;
-        $user->prenom = $user->prenom;
-        $user->email = $user->email;
-        $user->password = Hash::make($request->password);
-        $user->api_token = Str::random(100);
-        $user->remember_token = '';
-        $user->register_token = '';
-        $user->email_verified_at = $now;
-        $user->registered = 1;
-        $user->save();
-
-        return redirect('/login');
+        return redirect(RouteServiceProvider::HOME);
     }
 }
