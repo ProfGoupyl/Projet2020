09f3a6bc9ca90d712eea90b0e4ac453a91796645
clem <remy.clementine@ifosup.wavre.be>
2021-01-22 19:14:02 +0100

    diverses modif scss

--- projet_2020/public/js/app.js
+++ projet_2020/public/js/app.js
@@ -61579,11 +61579,13 @@ var render = function() {
           return _c("li", { key: module.id }, [
             _c("details", [
               _c("summary", { staticClass: "faq_question" }, [
-                _vm._v(" " + _vm._s(module.question) + " ")
+                _vm._v(" " + _vm._s(module.question)),
+                _c("a")
               ]),
               _vm._v(" "),
               _c("p", { staticClass: "faq_question_hidden" }, [
-                _vm._v(" " + _vm._s(module.reponse) + " ")
+                _vm._v(" " + _vm._s(module.reponse)),
+                _c("a")
               ])
             ])
           ])
@@ -74634,10 +74636,10 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /Applications/MAMP/htdocs/ifosup/projet-web-dynamique/Projet2020/projet_2020/resources/js/app.js */"./resources/js/app.js");
-__webpack_require__(/*! /Applications/MAMP/htdocs/ifosup/projet-web-dynamique/Projet2020/projet_2020/resources/sass/app.scss */"./resources/sass/app.scss");
-__webpack_require__(/*! /Applications/MAMP/htdocs/ifosup/projet-web-dynamique/Projet2020/projet_2020/resources/sass/default.scss */"./resources/sass/default.scss");
-module.exports = __webpack_require__(/*! /Applications/MAMP/htdocs/ifosup/projet-web-dynamique/Projet2020/projet_2020/resources/css/app.css */"./resources/css/app.css");
+__webpack_require__(/*! /Applications/MAMP/htdocs/Projet2020/projet_2020/resources/js/app.js */"./resources/js/app.js");
+__webpack_require__(/*! /Applications/MAMP/htdocs/Projet2020/projet_2020/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /Applications/MAMP/htdocs/Projet2020/projet_2020/resources/sass/default.scss */"./resources/sass/default.scss");
+module.exports = __webpack_require__(/*! /Applications/MAMP/htdocs/Projet2020/projet_2020/resources/css/app.css */"./resources/css/app.css");
 
 
 /***/ })
