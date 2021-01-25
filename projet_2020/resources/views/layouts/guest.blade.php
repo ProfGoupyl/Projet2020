d52fd7f84381f92024d9f916dcbbcbc79ae7a246
clem <remy.clementine@ifosup.wavre.be>
2021-01-25 17:11:03 +0100

    accept change

--- projet_2020/resources/views/layouts/guest.blade.php
+++ projet_2020/resources/views/layouts/guest.blade.php
@@ -8,17 +8,15 @@
 
     <title>{{ config('app.name', 'Laravel') }}</title>
 
-    <!-- Styles -->
-    <!-- <link rel="stylesheet" href="{{ asset('css/app.css') }}"> -->
-    <link rel="stylesheet" href="{{ asset('css/default.css') }}">
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
-</head>
-
-<body>
-    <div class="font-sans text-gray-900 antialiased">
-        {{ $slot }}
-    </div>
-</body>
-
+        <!-- Styles -->
+        <!-- <link rel="stylesheet" href="{{ asset('css/app.css') }}"> -->
+        <link rel="stylesheet" href="{{ asset('css/default.css') }}">
+        <!-- Scripts -->
+        <script src="{{ asset('js/app.js') }}" defer></script>
+    </head>
+    <body>
+        <div class="font-sans text-gray-900 antialiased">
+            {{ $slot }}
+        </div>
+    </body>
 </html>
