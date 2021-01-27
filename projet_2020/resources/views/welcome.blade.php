311cee454e7815ecb050ef2ba62d77045158ccd6
Waryen <jonathangomand@gmail.com>
2021-01-27 16:13:40 +0100

    add: Contenu page d'accueil

--- projet_2020/resources/views/welcome.blade.php
+++ projet_2020/resources/views/welcome.blade.php
@@ -1,5 +1,32 @@
 @extends('layouts.default')
 
 @section('content')
-    
+    <h1>Bienvenue sur <strong>Goupyl Education</strong></h1>
+    <p>
+        Salut, je m'appelle Rudy Renard et je suis formateur à l'IFOSUP et propriétaire du site goupyl.com,. Sur ce site vous trouverez mes cours en vidéo qui vous permettront de progresser principalement en programmation.
+    </p>
+    <p>
+        Vous verrez à quel cours vous êtes inscrit (IFOSUP) mais également en élève libre ( sur demande )
+    </p>
+    <p>
+        Ce site a été réalisé dans le cadre du module 5XDYN - projet web dynamique 2020-2021- par les étudiants dont voici la liste : 
+    </p>
+    <ul>
+        <li>GOMAND Jonathan - <a href="https://www.linkedin.com/in/jonathan-gomand-9690761b7/" target="_blank">LinkedIn</a></li>
+        <li>LEBEGUE Sébastien</li>
+        <li>AMIR Ilham</li>
+        <li>PERRIARD Céline</li>
+        <li>CHAOUNI Saïd</li>
+        <li>DEMOLDER Thierry</li>
+        <li>SCORNEAUX Romain</li>
+        <li>REMY Clémentine</li>
+        <li>CHAVALLE Céline</li>
+        <li>MARCHAND François</li>
+        <li>MILICAN Nicolas</li>
+        <li>ROULEZ Antoine</li>
+        <li>LEGRAND Florian</li>
+        <li>GUERRERA Hugo</li>
+        <li>SIMONET Xavier</li>
+        <li>GENDARME Jo</li>
+    </ul>
 @endsection
