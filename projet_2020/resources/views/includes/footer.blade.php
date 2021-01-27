b599d5fc3ac19329b18024849904128e17c30dde
Waryen <jonathangomand@gmail.com>
2021-01-27 14:35:40 +0100

    add: liens mentions légales dans le footer

--- projet_2020/resources/views/includes/footer.blade.php
+++ projet_2020/resources/views/includes/footer.blade.php
@@ -1,1 +1,11 @@
-<div id="copyright text-right" class="footer">© Copyright 2020 Front-end MGR</div>
+<div class="footer">
+    <h3>Mentions légales</h3>
+    <nav>
+        <ul>
+            <li><a href="/charte-vie-privee" target="_blank">Charte vie privée</a></li>
+            <li><a href="/cookies" target="_blank">Cookies</a></li>
+            <li><a href="/droits-utilisations" target="_blank">Droits d'utilisations</a></li>
+            <li><a href="/reglement-interieur-etudiant" target="_blank">Réglement intérieur étudiant</a></li>
+        </ul>
+    </nav>
+</div>
