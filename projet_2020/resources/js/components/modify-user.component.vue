d4e8adc02301a28a75fe90728cd99bb395b26ed6
Morphee-creator <milican.2405.student@ifosupwavre.be>
2021-01-27 16:12:21 +0100

    modify-user fonctionnel

--- projet_2020/resources/js/components/modify-user.component.vue
+++ projet_2020/resources/js/components/modify-user.component.vue
@@ -1,91 +1,99 @@
 <template>
-    <div>
-        <section class="prof">
-            <h1>Votre profil:</h1>
-            <form method="patch" id="formSubmit" class="userProfil">
-                <input type="hidden" name="_token" v-bind:value="token">
-                <div>
-                    <p>
-                        <label for="firstname">First Name:</label>
-                        <input type="text" name="firstname" v-model="userPrenom">
-                    </p>
-                    <p>
-                        <label for="lastname">Last Name:</label>
-                        <input type="text" name="lastname" v-model="userName">
-                    </p>
-                    <p>
-                        <label for="email">Email address:</label>
-                        <input type="email" name="email" v-model="userEmail">
-                    </p>
-                </div>
-                <div>
-                    <p>
-                        <label for="currentPicture">Photo de profil actuelle:</label>
-                        <img src="" alt="" class="UserImage" width="50px" height="50px">
-                    </p>
-                </div>
-            </form>
+  <div>
+    <section class="prof">
+      <h1>Votre profil:</h1>
+      <form v-on:submit.prevent="submit" method="patch" class="userProfil">
+        <input type="hidden" name="_token" v-bind:value="token" />
+        <div>
+          <p>
+            <label for="prenom">First Name:</label>
+            <input type="text" name="prenom" v-model="userPrenom" />
+          </p>
+          <p>
+            <label for="name">Last Name:</label>
+            <input type="text" name="name" v-model="userName" />
+          </p>
+          <p>
+            <label for="email">Email address:</label>
+            <input type="email" name="email" v-model="userEmail" />
+          </p>
+        </div>
+        <div>
+          <p>
+            <label for="currentPicture">Photo de profil actuelle:</label>
+            <img src="" alt="" class="UserImage" width="50px" height="50px" />
+          </p>
 
-            <button id="modifier" class="btn btn-primary" type="submit" @click="submit"><i class="fas fa-pen fa-lg"></i></button>
-            <button><a href="/user">Annuler</a></button>
-        </section>
-    </div>
+          <input type="file" name="userPhoto" v-on:change="selectImage" />
+        </div>
+        <button
+          v-on:click="submitFile()"
+          id="modifier"
+          class="btn btn-primary"
+          type="submit"
+        >
+          <i class="fas fa-pen fa-lg"></i>
+        </button>
+        <button><a href="/user">Annuler</a></button>
+      </form>
+    </section>
+  </div>
 </template>
 
 <script>
-    export default {
-        props: ['userInfos'],
-        data() {
-            return {
-                token: document.querySelector('#token').getAttribute('content'),
-                url: document.querySelector('#envUrl').getAttribute('content'),
-                userId: this.userInfos.id,
-                userName: this.userInfos.name,
-                userPrenom: this.userInfos.prenom,
-                userEmail: this.userInfos.email,
-                userPseudo: this.userInfos.pseudo,
-                userPhoto: null,
-                send: null,
-                uploadSucces: false,
-                uploadFail: false,
-            }
-        },
-        methods: {
-            submit() {
-                document.getElementById('formSubmit').submit()
-                
-                axios.patch(`${this.url}/api/users/${this.userInfos.id}/?api_token=${this.userInfos.api_token}`
-                , {
-                    name: this.userName,
-                    prenom: this.userPrenom,
-                    email: this.userEmail,
-                    pseudo: this.userPseudo,
-                })
-                .then(response => this.send = true)
-                .catch(error => this.send = false)
-            },
-            selectImage(event) {
-                this.userPhoto = event.target.files[0]
-            },
-            uploadImage(event) {
-                event.preventDefault()
-                let currentObj = this
+export default {
+  props: ["userInfos"],
+  data() {
+    return {
+      token: document.querySelector("#token").getAttribute("content"),
+      url: document.querySelector("#envUrl").getAttribute("content"),
+      userId: this.userInfos.id,
+      userName: this.userInfos.name,
+      userPrenom: this.userInfos.prenom,
+      userEmail: this.userInfos.email,
+      userPseudo: this.userInfos.pseudo,
+      userPhoto: null,
+      send: null,
+      uploadSucces: false,
+      uploadFail: false,
+    };
+  },
+  methods: {
+    submit() {
+      axios
+        .patch(
+          `${this.url}/api/users/${this.userInfos.id}/?api_token=${this.userInfos.api_token}`,
+          {
+            name: this.userName,
+            prenom: this.userPrenom,
+            email: this.userEmail,
+            pseudo: this.userPseudo,
+          }
+        )
+        .then((response) => console.log(response))
+        .catch((error) => console.log(error));
+    },
 
-                const config = {
-                    headers: {'content-type': 'multipart/form-data'}
-                }
+    submitFile() {
+      let currentObj = this;
 
-                const fd = new FormData()
-                fd.append('image', this.userPhoto)
+      const fd = new FormData();
+      fd.append("image", this.userPhoto);
 
-                axios.post('uploadImage', fd, config)
-                    .then(function(response) {
-                        currentObj.uploadSucces = response.data.success
-                    })
-                    .catch(function(error) {
-                        currentObj.uploadFail = error
-                    })
-            }
-        },
-    }
+      axios
+        .post("uploadImage", fd, {
+          headers: { "content-type": "multipart/form-data" },
+        })
+        .then(function (response) {
+          currentObj.uploadSucces = response.data.success;
+        })
+        .catch(function (error) {
+          currentObj.uploadFail = error;
+        });
+    },
+    selectImage(event) {
+      this.userPhoto = event.target.files[0];
+    },
+  },
+};
 </script>
