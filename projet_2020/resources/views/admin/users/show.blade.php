687823023242c4ff902068d4b23a526e0f645ee1
Frozen07 <Florian31.Legrand@gmail.com>
2021-02-01 14:38:57 +0100

    fix affichage user-show

--- projet_2020/resources/views/admin/users/show.blade.php
+++ projet_2020/resources/views/admin/users/show.blade.php
@@ -1,26 +1,29 @@
-
 @extends('layouts.default')
 @section('content')
-
 <section>
-		<h1>Liste des cours de {{$user->name}}</h1>
-		<table class="listecours">
-			<thead>
-				<tr>
-					<th>Nom du cours</th>
-					<th>Date de début</th>
-					<th>Date de fin</th>
-				</tr>
-			</thead>
-			<tbody>
+    <h1>Liste des cours de {{$user->name}}</h1>
+    <table class="listecours">
+        <thead>
+            <tr>
+                <th>Nom du cours</th>
+                <th>Date de début</th>
+                <th>Date de fin</th>
+            </tr>
+        </thead>
+        <tbody>
             @foreach($user->cours as $c)
-				<tr>
-					<td><a class="Cours" id="cours id1">{{$c->titre}}</a></td>
-					<td><p id="cours id1">{{$c->debut_du_cours}}</p></td>
-                    <td><p id="cours id1">{{$c->fin_du_cours}}</p></td>
-                </tr>
-                </tbody>
+            <tr>
+                <td><a class="Cours" id="cours id1">{{$c->titre}}</a></td>
+                <td>
+                    <p id="cours id1">{{$c->debut_du_cours}}</p>
+                </td>
+                <td>
+                    <p id="cours id1">{{$c->fin_du_cours}}</p>
+                </td>
+            </tr>
             @endforeach
+        </tbody>
+    </table>
 </section>
-    
+
 @stop
