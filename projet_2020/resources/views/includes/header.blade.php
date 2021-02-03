02f9d3d4fa7f604c549ceb0c59422cea4b8c1e57
roulette091 <roulez.0901.student@ifosupwavre.be>
2021-02-03 17:50:13 +0100

    changement header

--- projet_2020/resources/views/includes/header.blade.php
+++ projet_2020/resources/views/includes/header.blade.php
@@ -63,6 +63,40 @@
                 @endif
             </ul>
 
+            <li class="items hide">   
+                @if (Auth::user())
+                @if (Auth::user() && Auth::user()->admin === 1)
+                <li>
+                    @if(!\Request::is('admin/*'))
+                    <a href="{{ url('/admin') }}">Dashboard</a>
+                    @endif
+                    <form method="POST" action="{{ route('logout') }}">
+                        @csrf
+                        <x-dropdown-link :href="route('logout')" onclick="event.preventDefault();
+                                                    this.closest('form').submit();">
+                            {{ __('Logout') }}
+                        </x-dropdown-link>
+                    </form>
+                    <p>Vous êtes connecté(e) en tant que {{ Auth::user()->name }} {{ Auth::user()->prenom }} (Admin)</p>
+                </li>
+                @endif
+                @if (Auth::user() && Auth::user()->admin === 0)
+                <li>
+                    <form method="POST" action="{{ route('logout') }}">
+                        @csrf
+                        <x-dropdown-link :href="route('logout')" onclick="event.preventDefault();
+                                                    this.closest('form').submit();">
+                            {{ __('Logout') }}
+                        </x-dropdown-link>
+                    </form>
+                    <p>Vous êtes connecté(e) en tant que <span>{{ Auth::user()->name }} {{ Auth::user()->prenom }}</span></p>
+                </li>
+                @endif
+                @else
+                @endif
+            </li>   
+          
+
         </li>
         <li class="burger"><a href="#"><i class="fas fa-bars"></i></a></li>
     </ul>
