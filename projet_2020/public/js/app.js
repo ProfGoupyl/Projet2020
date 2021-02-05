75e911641fe223b64c11f2fb72486564f2b37aac
clem <remy.clementine@ifosup.wavre.be>
2021-02-05 16:25:06 +0100

    fix version mobile

--- projet_2020/public/js/app.js
+++ projet_2020/public/js/app.js
@@ -62118,7 +62118,7 @@ var render = function() {
   var _h = _vm.$createElement
   var _c = _vm._self._c || _h
   return _c("div", [
-    _c("section", [
+    _c("section", { attrs: { id: "user" } }, [
       _c("h1", [_vm._v("Liste des cours")]),
       _vm._v(" "),
       _c("table", { staticClass: "listecours" }, [
